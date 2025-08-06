8e6ff24c869bbae52c04c6814c0ca0a20127fe2b
TianpeiLuke <tianpei@umich.edu>
2025-08-06 12:08:02 -0700

    update version

--- src/cursus/__version__.py
+++ src/cursus/__version__.py
@@ -1,6 +1,6 @@
 """Version information for Cursus."""
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __title__ = "cursus"
 __description__ = "Automatic SageMaker Pipeline Generation from DAG Specifications"
 __author__ = "Tianpei Xie"
