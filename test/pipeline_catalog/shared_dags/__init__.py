a22735408ef888ddbac15cc5e3a10b2d0f5f6b37
TianpeiLuke <tianpei@umich.edu>
2025-09-18 09:40:08 -0700

    rm old pipeline catalog tests

--- test/pipeline_catalog/shared_dags/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Unit tests for pipeline catalog shared DAGs.
-
-This package contains comprehensive unit tests for all utilities and helper
-functions in the cursus/pipeline_catalog/shared_dags module, following the same
-folder structure and testing all functionality.
-"""
